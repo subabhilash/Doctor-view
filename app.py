e57065f8fab7e17dc4f793832dcf25b196fe3590
subabhilash <163818572+subabhilash@users.noreply.github.com>
2024-04-09 08:32:34 +0000

    final

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, render_template
 from database import load_db
 
+from flask import request
+import mysql.connector
+from database import mydb
+
 app = Flask(__name__)
 
 
@@ -11,6 +15,21 @@ def hello_world():
                         patients=patients
                         )
 
+@app.route("/submit", methods=["POST"])
+def submit_form():
+    name = request.form["name"]
+    age = request.form["age"]
+    date = request.form["date"]
+    time = request.form["time"]
+    tablet = request.form["tablet"]
+
+    cursor = mydb.cursor()
+    sql = "INSERT INTO tablets (p_date,p_time,p_name,age,tablet) VALUES (%s, %s, %s, %s, %s)"
+    val = (date, time,name, age, tablet)
+    cursor.execute(sql, val)
+    mydb.commit()
+
+    return "Form submitted successfully"
 
 
 
